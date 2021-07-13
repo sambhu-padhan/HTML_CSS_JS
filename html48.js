4c416175c53286b0ccf3a32d14c1ff7bd3a17da8
SAMBHU PADHAN <85475719+sambhu-padhan@users.noreply.github.com>
2021-07-13 17:09:25 +0530

    Add files via upload

---
+++ html48.js
@@ -0,0 +1,15 @@
+console.log("this js tutorial.");
+
+name ="Sambhu";
+name1 = "Rajesh";
+name2 = "Ganesh";
+name3 = "Dev";
+
+function printname(name){
+console.log(name+" is a good boy.");
+
+}
+printname(name);
+printname(name1);
+printname(name2);
+printname(name3);
